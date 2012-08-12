f442c1cd6c3a8a638a22798d9a17b98bde06c03a
Bibhas C Debnath <iambibhas@gmail.com>
2012-08-13 00:06:18 +0530

    First

---
+++ domainset.sh
@@ -0,0 +1,31 @@
+echo "Settings up domain name $1"
+echo "At the location $2"
+
+touch $1
+salias=`echo $1 | cut -d'.' -f1`
+echo "<VirtualHost *:80>
+    ServerName $1
+    ServerAlias $salias
+    DocumentRoot $2
+
+    <Directory $2/>
+        Options Indexes FollowSymLinks MultiViews
+        AllowOverride all
+        Order allow,deny
+        allow from all
+    </Directory>
+
+    ErrorLog ${APACHE_LOG_DIR}/$salias-error.log
+    CustomLog ${APACHE_LOG_DIR}/access.log combined
+</VirtualHost>" > $1
+
+touch hosts
+echo "127.0.0.1       $1" > hosts
+sudo cat /etc/hosts >> hosts
+
+sudo mv hosts /etc/hosts
+
+sudo a2dissite $1
+sudo a2ensite $1
+
+sudo service apache2 reload
